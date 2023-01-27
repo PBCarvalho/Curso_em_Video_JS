a667f7c69975811cd2dc62ead3c1e2da17c554c0
Peterson Carvalho <peterson.carvalho20@gmail.com>
2023-01-27 15:58:26 -0300

    Estudando JS

--- aula014ex/ex017/script.js
+++ aula014ex/ex017/script.js
@@ -1,1 +1,17 @@
-alert('Olá!')
+function tabuada() {
+    let numero = document.getElementById('numTabuada').value
+    let tab = document.querySelector('#tab')
+
+    if(numero == 0) {
+        alert('Por favor, digite um número!')
+    } else {
+        tab.innerHTML = ''
+
+        for(let c = 1; c <= 10; c++) {
+            let item = document.createElement('option')
+            item.text = `${numero} x ${c} = ${numero * c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+        }
+    }
+}
---
+++ aula014ex/ex017/teste.js
@@ -0,0 +1,7 @@
+let numero = 5
+
+for(let c = 0; c <= 10; c++) {
+    let resultado = c * numero
+
+    console.log(`${numero} X ${c} = ${resultado}`)
+}
